e92979ff86a102aa81eacf3014bdb4072330ff92
Yohan Boujon <yo.boujon@gmail.com>
2026-08-02 19:22:48 +0200

    Using SHT3X instead of DHT sensor for temperature.

--- embedded/main/main.c
+++ embedded/main/main.c
@@ -16,12 +16,13 @@
 #include "lwip/sockets.h"
 #include "lwip/inet.h"
 
-#include "dht.h"
+#include "sht3x.h"
 
-#define THERM_R0 100000.0f
-#define THERM_BETA 4255.0f
-#define KELVIN 273.15f
-#define THERM_T0 (25.0f + KELVIN)
+#define SDA_GPIO GPIO_NUM_11
+#define SCL_GPIO GPIO_NUM_10
+#define I2C_PORT 0
+#define SHT3X_ADDR 0x44
+#define TIME_DELTA_MS 5000
 
 typedef struct __attribute__((packed)) yoyotemp_packet {
     uint16_t humidity;
@@ -29,6 +30,7 @@ typedef struct __attribute__((packed)) yoyotemp_packet {
 } yoyotemp_packet_t;
 
 static yoyotemp_packet_t pkt;
+static sht3x_t dev;
 
 static const char *TAG = "wifi";
 static EventGroupHandle_t wifi_event_group;
@@ -62,26 +64,11 @@ void send_udp_packet(void)
 
 void app_main(void)
 {
-
-    // ADC shit
-    adc_oneshot_unit_handle_t adc;
-    adc_oneshot_unit_init_cfg_t init_cfg = {
-        .unit_id = ADC_UNIT_1,
-    };
-    adc_cali_handle_t cali_handle;
-    adc_cali_curve_fitting_config_t cali_config = {
-        .unit_id = ADC_UNIT_1,
-        .chan = ADC_CHANNEL_6,
-        .atten = ADC_ATTEN_DB_12,
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-    };
-    adc_oneshot_chan_cfg_t cfg = {
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-        .atten = ADC_ATTEN_DB_12,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_cfg, &adc));
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc, ADC_CHANNEL_6, &cfg));
-    ESP_ERROR_CHECK(adc_cali_create_scheme_curve_fitting(&cali_config, &cali_handle));
+    // I2C shit
+    ESP_ERROR_CHECK(i2cdev_init());
+    memset(&dev, 0, sizeof(sht3x_t));
+    ESP_ERROR_CHECK(sht3x_init_desc(&dev, SHT3X_ADDR, I2C_PORT, SDA_GPIO, SCL_GPIO));
+    ESP_ERROR_CHECK(sht3x_init(&dev));
 
     // Wi-Fi shit
     esp_err_t ret = nvs_flash_init();
@@ -111,41 +98,33 @@ void app_main(void)
     wifi_event_group = xEventGroupCreate();
     esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL);
 
+    const uint32_t max_sample = (uint32_t)(ceil(6e5/TIME_DELTA_MS));
     uint32_t sample = 0;
-    int16_t humidity, unused;
-    int raw, voltage_mv;
-    float temperature, avg_temp, total_temp = 0;
+    float temperature, humidity, avg_temp, total_temp = 0;
 
     xEventGroupWaitBits(wifi_event_group, IP_ASSIGNED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
     while (1)
     {
-        ESP_ERROR_CHECK(adc_oneshot_read(adc, ADC_CHANNEL_6, &raw));
-        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(cali_handle, raw, &voltage_mv));
-
-        const float voltage = voltage_mv / 1000.0f;
-        const float resistor = (3.3f - voltage) * THERM_R0 / voltage;
-        const float tempK = 1.0f / (1.0f / THERM_T0 + logf(resistor / THERM_R0) / THERM_BETA);
-        temperature = tempK - KELVIN;
-        total_temp += temperature;
-        sample++;
-        avg_temp = total_temp / sample;
-
-        if (dht_read_data(DHT_TYPE_DHT11, GPIO_NUM_22, &humidity, &unused) == ESP_OK)
+        const esp_err_t res = sht3x_measure(&dev, &temperature, &humidity);
+        if (res == ESP_OK)
         {
-            printf("Humidity: %.1f%%   Average (%lu): %.3f°C   Actual: %.3f°C\n", humidity / 10.0f, sample, avg_temp, temperature);
+            total_temp += temperature;
+            sample++;
+            avg_temp = total_temp / sample;
+            printf("Humidity: %.1f%%   Average (%lu): %.3f°C   Actual: %.3f°C\n", humidity, sample, avg_temp, temperature);
         }
         else
         {
-            printf("Thermistor: %.3f°C\n", temperature);
+            printf("Measurement failed: %s (0x%x)\n", esp_err_to_name(res), res);
         }
 
-        if(sample == 600) {
-            pkt.humidity = humidity;
+        if(sample == max_sample) {
+            pkt.humidity = (uint16_t)(humidity*10.0);
             pkt.temp = avg_temp;
             send_udp_packet();
             sample = 0;
             total_temp = 0;
         }
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(TIME_DELTA_MS));
     }
 }
